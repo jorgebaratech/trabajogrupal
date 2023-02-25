a6ee960175479505b5baeb33a076bab18aea6ba1
jorgebaratech <93922396+jorgebaratech@users.noreply.github.com>
2023-02-25 11:01:59 +0100

    Add files via upload

---
+++ proyectgrupal/src/main/java/com/example/grupal/GrupalApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupalApplication.class, args);
+	}
+
+}
---
+++ proyectgrupal/src/test/java/com/example/grupal/GrupalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
